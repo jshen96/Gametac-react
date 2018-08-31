be56d8e1a6b0b7dcb61fa45f5a0290fd25a4d292
jshen96 <ongjieshen@gmail.com>
2018-08-31 11:50:26 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<div>
+
+<h1>OLA</h1>
+  </div>, document.getElementById('root'));
+registerServiceWorker();
